c03d3f4e0fcf63380ec34b0e2df48e49a57bdef0
Barka <esma.barka@lamyliaisons.fr>
2026-05-20 11:57:16 +0200

    add k6 performance pipeline

---
+++ gatling/k6-test.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export default function () {
+  let res = http.get('https://httpbin.org/get');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
