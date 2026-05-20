5bace464f80672a793c130bff697e2e5cd79a69b
Barka <esma.barka@lamyliaisons.fr>
2026-05-20 12:10:30 +0200

    fix gatling structure

---
+++ src/test/scala/BasicSimulation.scala
@@ -0,0 +1,19 @@
+﻿import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("https://httpbin.org")
+
+  val scn = scenario("Basic Test")
+    .exec(
+      http("GET request")
+        .get("/get")
+        .check(status.is(200))
+    )
+
+  setUp(
+    scn.inject(atOnceUsers(5))
+  ).protocols(httpProtocol)
+}
