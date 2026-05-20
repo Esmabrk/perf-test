62799a8c71985184a6e35d463fdf18c29753a079
Barka <esma.barka@lamyliaisons.fr>
2026-05-20 11:49:07 +0200

    add gatling performance tests

---
+++ gatling/src/test/scala/simulation/BasicSimulation.scala
@@ -0,0 +1,20 @@
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("https://httpbin.org")
+
+  val scn = scenario("Basic Load Test")
+    .exec(
+      http("GET request")
+        .get("/get")
+        .check(status.is(200))
+    )
+
+  setUp(
+    scn.inject(atOnceUsers(5))
+  ).protocols(httpProtocol)
+}
